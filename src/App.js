e40467469cc9a309d8f391fd7cbec680bcaed021
jyotip101 <jyotipatelfyit77100@gmail.com>
2021-09-29 19:07:41 +0530

    aded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import React, { useEffect, useState} from 'react'
 import Home from './homepage'
 // import './css/app.css'
 import Example from './example'
 function App() {
+
+  
+  const API_URL = `https://api.openweathermap.org/data/2.5/`
+  const API_KEY = '7c707d67bfaea395afd4f3cf375077ca' 
+
+  const ICON_URL = `http://openweathermap.org/img/wn/`
+  //  ${data.weather[0].icon}@2x.pn
+
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+  const [data, setData] = useState([])
+
+  useEffect(() =>{
+      const fetchData = async() => {
+          navigator.geolocation.getCurrentPosition( pos =>{
+              setLat(pos.coords.latitude)
+              setLon(pos.coords.longitude)
+          })
+
+     await fetch(`${API_URL}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+          .then(res => res.json())
+          .then(result => {
+              setData(result)
+              console.log(result)
+          })
+      }
+      fetchData()
+  }, [lat, lon])
+
+
   return (
-    <div className="App">  
-      <Home/>
-      {/* <Example/> */}
+    <div className="App"> 
+    { (typeof data.main != 'undefined') ? (
+          
+      <Home datas={data}/>  
+
+      ) : (
+            <div> </div>
+        ) } 
     </div>
   )
 }
--- src/homepage.js
+++ src/homepage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react'
+import React from 'react'
 import {
     Card,Button,Label
 
@@ -7,46 +7,18 @@ import 'semantic-ui-css/semantic.min.css'
 import moment from 'moment'
 
 import './css/app.css'
-const HomePage = () =>{
-
-    const API_URL = `https://api.openweathermap.org/data/2.5/`
-    const API_KEY = '7c707d67bfaea395afd4f3cf375077ca'
-    // const ICON_URL = 'https://openweathermap.org/img/w'
+const HomePage = (datas) =>{
  
-    const ICON_URL = `http://openweathermap.org/img/wn/`
-    //  ${data.weather[0].icon}@2x.pn
-
-    const [lat, setLat] = useState([])
-    const [lon, setLon] = useState([])
-    const [data, setData] = useState([])
-
-    useEffect(() =>{
-        const fetchData = async() => {
-            navigator.geolocation.getCurrentPosition( pos =>{
-                setLat(pos.coords.latitude)
-                setLon(pos.coords.longitude)
-            })
-
-       await fetch(`${API_URL}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-            .then(res => res.json())
-            .then(result => {
-                setData(result)
-                console.log(result)
-            })
-        }
-        fetchData()
-    }, [lat, lon])
-
     return(
-    <> 
-            <Card>
+    <>      
+             <Card>
                 <Card.Content>
                     <Card.Header textAlign='center'>
                         <p> 
                            <span>  {moment().format("LT")},   </span> 
                            <span>  {moment().format("Do MMMM YYYY")}</span> 
                     </p><p>
-                            {data.sys.country}, {data.name} 
+                            {datas.sys.country}, {datas.name} 
                             
                         </p>
                     </Card.Header>
@@ -54,18 +26,18 @@ const HomePage = () =>{
             </Card>  
             <Card>
                 <Card.Content textAlign='center'>
-                    <img width="50%" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
-                    <h3>{data.weather[0].main}   </h3>
-                    <p>{data.weather[0].description}</p>
+                    <img width="50%" src={`http://openweathermap.org/img/wn/${datas.weather[0].icon}@2x.png`} />
+                    <h3>{datas.weather[0].main}   </h3>
+                    <p>{datas.weather[0].description}</p>
                 </Card.Content>
             </Card>
             <Card>
                 <Card.Content textAlign='center'>
                     <p>
                         <i width='25px' class="fas fa-temperature-low"></i>
-                        { ((data.main.temp_min ) - 273.15) + "C" } 
+                        { ((datas.main.temp_min ) - 273.15) + "C" } 
                         <i class="fas fa-temperature-high"></i>
-                        {((data.main.temp_max  ) - 273.15) + "C" }
+                        {((datas.main.temp_max  ) - 273.15) + "C" }
                     </p> 
                 </Card.Content>
             </Card> 
@@ -74,7 +46,8 @@ const HomePage = () =>{
                     <a   href= "https://github.com/jyotip101/Weather-app-in-js/blob/main/src/script.js">
                         <i className='fab fa-github' /> source code
                     </a> 
-            </Button>
+            </Button> 
+            
 </>
     )
 }
